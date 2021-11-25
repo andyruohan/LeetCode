8375dcfe5ad19cfb2c77c0be4227641e61ba75db
andyruohan <237191099@qq.com>
2021-11-25 22:33:39 +0800

    优化点：1. 记录right指针的下标，而非累加计数; 2. left指针直接滑动到上一次字符出现的位置，不必逐步滑

--- src/problem3/Solution.java
+++ src/problem3/Solution.java
@@ -5,30 +5,33 @@ package problem3;
  * 1. 通过滑动right指针遍历字符串，已遍历的字符放进ASCII数组并计数
  * 2. 某个字符数量大于1时滑动left指针，直至滑动到该字符数不大于1为止
  * 3. 取滑动过程中，right-left+1的最大值即为返回结果
+ *
+ * 优化：
+ * 1. 记录right指针的下标，而非累加计数
+ * 2. left指针直接滑动到上一次字符出现的位置，不必逐步滑
  */
 class Solution {
     public static int lengthOfLongestSubstring(String s) {
-        int[] chars = new int[128];
+        Integer[] chars = new Integer[128];
         int left = 0;
         int right = 0;
         int res = 0;
 
         while (right < s.length()) {
             char r = s.charAt(right);
-            chars[r]++;
 
-            while (chars[r] > 1) {
-                char l = s.charAt(left);
-                chars[l]--;
-                left++;
+            Integer index = chars[r];
+            if (index != null && index >= left && index < right) {
+                left = index + 1;
             }
 
             res = Math.max(res, right - left + 1);
+            chars[r] = right;
             right++;
         }
         return res;
     }
     public static void main(String[] args) {
-        System.out.println(Solution.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(Solution.lengthOfLongestSubstring("abcabcbb"));
     }
 }
