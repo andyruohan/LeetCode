56b94ec07d987cd28d64c62a915106de6f3316c4
andyruohan <237191099@qq.com>
2021-11-25 21:50:45 +0800

    修复left滑动指针

--- src/problem3/Solution.java
+++ src/problem3/Solution.java
@@ -1,7 +1,13 @@
-package problem4;
+package problem3;
 
+/**
+ * 解题思路：
+ * 1. 通过滑动right指针遍历字符串，已遍历的字符放进ASCII数组并计数
+ * 2. 某个字符数量大于1时滑动left指针，直至滑动到该字符数不大于1为止
+ * 3. 取滑动过程中，right-left+1的最大值即为返回结果
+ */
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public static int lengthOfLongestSubstring(String s) {
         int[] chars = new int[128];
         int left = 0;
         int right = 0;
@@ -11,7 +17,7 @@ class Solution {
             char r = s.charAt(right);
             chars[r]++;
 
-            if (chars[r] > 1) {
+            while (chars[r] > 1) {
                 char l = s.charAt(left);
                 chars[l]--;
                 left++;
@@ -21,5 +27,8 @@ class Solution {
             right++;
         }
         return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(Solution.lengthOfLongestSubstring("pwwkew"));
     }
 }
