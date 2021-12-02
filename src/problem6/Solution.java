9d646c1249ee9ee89a7b8acf28ce715977081f5c
andyruohan <237191099@qq.com>
2021-12-02 21:40:46 +0800

    1. format了代码 2. 添加对末尾不足L时的处理 surprise：提交的时候才发现，输出一行字符串就可以，不需要输出一个类二维数组的字符串

--- src/problem6/Solution.java
+++ src/problem6/Solution.java
@@ -8,27 +8,39 @@ class Solution {
 
         int length = s.length();
         int numL = length / (2 * numRows - 2);
-        char[][] str = new char[numRows][numL * (numRows - 1)];
-        for (int j = 0; j < numL; j++) {
+        int lastCharNum = length % (2 * numRows - 2);
+        int totalColNum = lastCharNum < numRows ? numL * (numRows - 1) + 1: numL * (numRows - 1) + 1 + (lastCharNum - numRows);
+        char[][] str = new char[numRows][totalColNum];
+        for (int j = 0; j < numL + 1; j++) {
             for (int i = 0; i < 2 * numRows - 2; i++) {
                 if (i < numRows) {
                     int row = i;
-                    int col = j*(numRows-1);
-                    int index = i+j*(2*numRows-2);
+                    int col = j * (numRows - 1);
+                    int index = i + j * (2 * numRows - 2);
+                    if (index >= length) break;
                     str[i][col] = s.charAt(index);
                 } else {
-                    int row = 2*numRows-(i+1)-1;
-                    int col = j*(numRows-1)+(i+1-numRows);
-                    int index = j*(2*numRows-2)+i;
+                    int row = 2 * numRows - (i + 1) - 1;
+                    int col = j * (numRows - 1) + (i + 1 - numRows);
+                    int index = j * (2 * numRows - 2) + i;
+                    if (index >= length) break;
                     str[row][col] = s.charAt(index);
                 }
             }
         }
 
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < totalColNum; j++) {
+                if(str[i][j]!='\u0000') {
+                    sb.append(str[i][j]);
+                }
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("PAYPALISHIRING", 4));
     }
 }
