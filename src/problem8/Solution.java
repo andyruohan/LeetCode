39ca64d1cfdda5538707c9ab6b5c44050ebebecc
andyruohan <237191099@qq.com>
2021-12-07 22:41:15 +0800

    兼容更多算例，但对3.1415926等带小数点的数字，仍无法处理

--- src/problem8/Solution.java
+++ src/problem8/Solution.java
@@ -9,13 +9,26 @@ public class Solution {
 
         Integer left = null;
         Integer right = null;
+        Boolean alreadyHasDigt = false;
+        Character signFlag = null;
         for (int i = 0; i < length; i++) {
             if (' ' != s.charAt(i)) {
                 if ((s.charAt(i) == '-' || s.charAt(i) == '+') && i != length - 1 && left == null) {
-                    left = i;
+                    signFlag = s.charAt(i);
                     continue;
                 }
                 if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+                    //处理"00000-42a1234"这样的前排为0的字符串
+                    if (alreadyHasDigt) {
+                        return 0;
+                    }
+
+                    //处理"  0000000000012345678"这样的前排为0的字符串
+                    if (s.charAt(i) == '0' && left == null) {
+                        continue;
+                    } else if (s.charAt(i) == '0' && signFlag != null) {
+                        continue;
+                    }
                     if (left == null) {
                         left = i;
                     } else {
@@ -25,12 +38,17 @@ public class Solution {
                     if (left == null) {
                         return 0;
                     } else if (left != null && right != null) {
-                        break;
-                    } else if (left != null && (s.charAt(left) == '-' || s.charAt(left) == '+')) {
+                        alreadyHasDigt = true;
+                    } else {
                         return 0;
-                    } else if (left != null && (s.charAt(left) != '-' && s.charAt(left) != '+')) {
-                        return Integer.valueOf(s.substring(left, left + 1));
                     }
+                }
+                if (i == length -1) {
+                    right = i;
+                }
+            } else {
+                if (left != null) {
+                    break;
                 }
             }
         }
@@ -42,6 +60,15 @@ public class Solution {
             return 0;
         }
         String str = s.substring(left, right + 1);
+        if (signFlag != null) {
+            str = signFlag + str;
+        }
+
+        //处理"20000000000000000000"这样的超长字符串
+        if (str.length() > 15) {
+            str = str.substring(0, 16);
+        }
+
         Long num = Long.valueOf(str);
         if (Integer.MIN_VALUE <= num && num <= Integer.MAX_VALUE) {
             return Integer.valueOf(str);
@@ -54,6 +81,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(myAtoi("1"));
+        System.out.println(myAtoi("-91283472332"));
     }
 }
