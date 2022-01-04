adc3b24255c4db3f102fc2da5910c7aad549cb69
andyruohan <237191099@qq.com>
2022-01-04 23:13:26 +0800

    answer to the problem24 Swap Nodes in Pairs

---
+++ src/problem24/Solution.java
@@ -0,0 +1,15 @@
+package problem24;
+
+public class Solution {
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+
+        return newHead;
+    }
+}
