9f8c3df70e590e6b593a20202a79505d6b3f9a7e
andyruohan <237191099@qq.com>
2022-01-04 23:52:21 +0800

    指针测试

--- src/problem24/Solution.java
+++ src/problem24/Solution.java
@@ -2,14 +2,27 @@ package problem24;
 
 public class Solution {
     public static ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2; //temp.next和node1分别代表不同的指针，temp.next指向的其他位置，并不影响node1的指向
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
         }
+        return dummyHead.next;
+    }
 
-        ListNode newHead = head.next;
-        head.next = swapPairs(newHead.next);
-        newHead.next = head;
-
-        return newHead;
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        System.out.println(head.val + " "+ head.next.val + " " + head.next.next.val + " "+ head.next.next.next.val);
+        ListNode newHead = swapPairs(head);
+        System.out.println(newHead.val + " "+ newHead.next.val + " " + newHead.next.next.val + " "+ newHead.next.next.next.val);
     }
 }
