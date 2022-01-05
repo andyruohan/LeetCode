2da8652864c33db5d9ad3b0909352e590f2868a2
andyruohan <237191099@qq.com>
2022-01-05 23:14:12 +0800

    优化problem21代码

--- src/problem23/Solution.java
+++ src/problem23/Solution.java
@@ -14,29 +14,19 @@ public class Solution {
     }
 
     private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {return list2;}
-        if (list2 == null) {return list1;}
-
         ListNode result = new ListNode(0);
         ListNode curPoint = result;
-        while (list1 != null || list2 != null) {
-            if (list1 == null) {
-                curPoint.next = list2;
-                break;
-            } else if (list2 == null) {
-                curPoint.next = list1;
-                break;
-            }
-
+        while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                curPoint.next = new ListNode(list1.val);
+                curPoint.next = list1;
                 list1 = list1.next;
             } else {
-                curPoint.next = new ListNode(list2.val);
+                curPoint.next = list2;
                 list2 = list2.next;
             }
             curPoint = curPoint.next;
         }
+        curPoint.next = list1 == null ? list2 : list1;
         return result.next;
     }
 }
