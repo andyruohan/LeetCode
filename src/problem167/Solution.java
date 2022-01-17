00000abdc101585cdcb8f0e17d953a2c16f527f8
andyruohan <237191099@qq.com>
2022-01-17 22:12:55 +0800

    answer to the problem167 Two Sum II - Input Array Is Sorted

---
+++ src/problem167/Solution.java
@@ -0,0 +1,20 @@
+package problem167;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (hashMap.containsKey(target - numbers[i])) {
+                return new int[] {hashMap.get(target - numbers[i]) + 1, i + 1};
+            }
+            hashMap.put(numbers[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[] {2,7,11,15}, 9));
+    }
+}
