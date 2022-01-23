fcf0b7abc8979cb77f0520241a38895c6635e159
andy_ruohan <237191099@qq.com>
2022-01-23 17:36:32 +0800

    answer to the problem695 Max Area of Island

---
+++ src/Algorithm_I/Day7/problem695/Solution.java
@@ -0,0 +1,33 @@
+package Algorithm_I.Day7.problem695;
+
+public class Solution {
+    public static int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static int dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != 1) {
+            return 0;
+        }
+
+        int curMaxArea = 1;
+        if (grid[x][y] == 1) {
+            grid[x][y] = 0;
+            return dfs(grid, x - 1, y) + dfs(grid, x + 1, y) + dfs(grid, x, y - 1) + dfs(grid, x, y + 1) + curMaxArea;
+        }
+
+        return curMaxArea;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        System.out.println(maxAreaOfIsland(grid));
+    }
+}
