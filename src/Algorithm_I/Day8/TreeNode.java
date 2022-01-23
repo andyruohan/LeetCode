82ccf59f1b632a04d56c5e7de638f0ee31fcf780
andy_ruohan <237191099@qq.com>
2022-01-23 21:22:00 +0800

    answer to the problem617 Merge Two Binary Trees

---
+++ src/Algorithm_I/Day8/TreeNode.java
@@ -0,0 +1,14 @@
+package Algorithm_I.Day8;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
