27eb283299c4bdd6a90c6bcfb3d81d916598fcb1
andyruohan <237191099@qq.com>
2022-01-24 23:44:33 +0800

    answer to the problem116 Populating Next Right Pointers in Each Node

---
+++ src/Algorithm_I/Day8/problem116/Solution.java
@@ -0,0 +1,18 @@
+package Algorithm_I.Day8.problem116;
+
+public class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        if (root.left != null) {
+            root.left.next = root.right;
+            root.right.next = root.next != null ? root.next.left : null;
+            connect(root.left);
+            connect(root.right);
+        }
+
+        return root;
+    }
+}
