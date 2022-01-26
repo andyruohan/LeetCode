3a84311ff577725cf125b0ee365d99f54cb1ee74
andyruohan <237191099@qq.com>
2022-01-26 23:31:15 +0800

    answer to the problem994 Rotting Oranges

---
+++ src/problem994/Solution.java
@@ -0,0 +1,23 @@
+package problem994;
+
+public class Solution {
+//    public int orangesRotting(int[][] grid) {
+//        if () {
+//
+//        }
+//    }
+//
+//    public int dfs(int[][] grid, int x, int y) {
+//
+//
+//        if (grid[x][y] == 2) {
+//            int rows = grid.length;
+//            int cols = grid[0].length;
+//            if (x >= 1 && grid[x - 1][y] == 1) grid[x - 1][y] = 2;
+//            if (x + 1 < rows && grid[x + 1][y] == 1) grid[x + 1][y] = 2;
+//            if (y >= 1 && grid[x][y - 1] == 1) grid[x][y - 1] = 2;
+//            if (y + 1 < cols && grid[x][y + 1] == 1) grid[x][y + 1] = 2;
+//        }
+//        return
+//    }
+}
