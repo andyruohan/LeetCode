be542670d2ddb1e677d59825d34123c73826a7b4
andyruohan <237191099@qq.com>
2022-01-26 23:43:44 +0800

    answer to the problem994 Rotting Oranges

---
+++ src/Algorithm_I/Day9/problem994/Solution.java
@@ -0,0 +1,59 @@
+package Algorithm_I.Day9.problem994;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                } else if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        int round = 0;
+        while (count > 0 && !queue.isEmpty()) {
+            round++;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                int[] orange = queue.poll();
+                int r = orange[0];
+                int c = orange[1];
+                if (r - 1 >= 0 && grid[r - 1][c] == 1) {
+                    grid[r - 1][c] = 2;
+                    count--;
+                    queue.add(new int[]{r - 1, c});
+                }
+                if (r + 1 < rows && grid[r + 1][c] == 1) {
+                    grid[r + 1][c] = 2;
+                    count--;
+                    queue.add(new int[]{r + 1, c});
+                }
+                if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+                    grid[r][c - 1] = 2;
+                    count--;
+                    queue.add(new int[]{r, c - 1});
+                }
+                if (c + 1 < cols && grid[r][c + 1] == 1) {
+                    grid[r][c + 1] = 2;
+                    count--;
+                    queue.add(new int[]{r, c + 1});
+                }
+            }
+        }
+
+        if (count > 0) {
+            return -1;
+        } else {
+            return round;
+        }
+    }
+}
