73673af15b66d39c3503bfa358c45a6e3ddee652
andy_ruohan <237191099@qq.com>
2022-01-30 20:48:24 +0800

    optimize problem231

--- src/Algorithm_I/Day13/problem231/Solution.java
+++ src/Algorithm_I/Day13/problem231/Solution.java
@@ -2,19 +2,7 @@ package Algorithm_I.Day13.problem231;
 
 public class Solution {
     public static boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        } else if (n == 1) {
-            return true;
-        }
-
-        while (n > 1) {
-            if (n % 2 != 0) {
-                return false;
-            }
-            n = n / 2;
-        }
-        return true;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 
     public static void main(String[] args) {
