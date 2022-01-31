831fe6569916eb4d01bbd3fb2f5fa04b9f808baa
andyruohan <237191099@qq.com>
2022-01-31 23:33:01 +0800

    answer to the problem190 with JDK

--- src/Algorithm_I/Day14/problem190/Solution.java
+++ src/Algorithm_I/Day14/problem190/Solution.java
@@ -4,8 +4,10 @@ public class Solution {
     public int reverseBits(int n) {
         int reverseN = 0;
         for (int i = 0; i < 32; i++) {
-            reverseN += (1 & (n >> i)) << (32 - i);
+            reverseN += (1 & (n >> i)) << (31 - i);
         }
         return reverseN;
+
+        //return Integer.reverse(n); //JDK源码
     }
 }
