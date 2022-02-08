0912a58865522cb6cc72342e9d143f9423e27807
andy_ruohan <237191099@qq.com>
2022-02-08 11:08:46 +0800

    answer to the problem82 Remove Duplicates from Sorted List II

---
+++ src/Algorithm_II/Day3/problem82/ListNode.java
@@ -0,0 +1,9 @@
+package Algorithm_II.Day3.problem82;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
