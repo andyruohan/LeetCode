cd76b8be4643f050c6da3d2d594ca34672575da7
andy_ruohan <237191099@qq.com>
2022-02-12 23:57:45 +0800

    optimize problem713 with reference

--- src/Algorithm_II/Day5/problem713/Solution.java
+++ src/Algorithm_II/Day5/problem713/Solution.java
@@ -2,44 +2,25 @@ package Algorithm_II.Day5.problem713;
 
 public class Solution {
     public static int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+
         int left = 0;
-        int right = 0;
-        int length = nums.length;
-
         int product = 1;
-        int count = 0;
-        for (int i = 0; i < length; i++) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
             product *= nums[i];
-            if (product < k) {
-                right = i;
-                count++;
-                break;
-            }
+            while (product >= k) product = product / nums[left++];
+            result += i - left + 1;
         }
-
-        while (left <= right) {
-            if (right + 1 < length) {
-                if (nums[right + 1] < k) {
-                    count++;
-                }
-                product *= nums[++right];
-                if (product >= k) {
-                    while (left < right && product / nums[left] < k) {
-                        product /= nums[left++];
-                        count++;
-                    }
-                } else {
-                    count++;
-                }
-            } else {
-                break;
-            }
-        }
-        return count;
+        return result;
     }
 
     public static void main(String[] args) {
-//        System.out.println(numSubarrayProductLessThanK(new int[] {10,9,10,4,3,8,3,3,6,2,10,10,9,3}, 19));
+        System.out.println(numSubarrayProductLessThanK(new int[] {10,9,10,4,3,8,3,3,6,2,10,10,9,3}, 19));
         System.out.println(numSubarrayProductLessThanK(new int[] {1, 1, 1}, 5));
+        System.out.println(numSubarrayProductLessThanK(new int[] {10,2,2,5,4,4,4,3,7,7}, 289));
+
     }
 }
