f83d9af72f9dc3feef76fbeef29cab3c4612db99
andy_ruohan <237191099@qq.com>
2022-02-13 21:58:06 +0800

    optimize problem153 Find Minimum in Rotated Sorted Array with reference

--- src/Algorithm_II/Day2/problem153/Solution.java
+++ src/Algorithm_II/Day2/problem153/Solution.java
@@ -2,28 +2,17 @@ package Algorithm_II.Day2.problem153;
 
 public class Solution {
     public static int findMin(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
         int begin = 0;
         int end = nums.length - 1;
-        int minValue = Integer.MAX_VALUE;
-        int index = 0;
-        while (begin <= end) {
-            index = (begin + end) / 2;
+        while (begin < end) {
+            int index = (begin + end) / 2;
             if (nums[begin] > nums[index]) {
-                minValue = Math.min(minValue, nums[index]);
-                end = index - 1;
-            } else if (nums[index] > nums[end]) {
-                minValue = Math.min(minValue, nums[begin]);
+                end = index;
+            } else {
                 begin = index + 1;
-            } else if (nums[begin] <= nums[index] && nums[index] <= nums[end]) {
-                minValue = Math.min(minValue, nums[begin]);
-                break;
             }
         }
-        return minValue;
+        return begin;
     }
 
 //    public static int findMinCore(int[] nums, int begin, int end) {
