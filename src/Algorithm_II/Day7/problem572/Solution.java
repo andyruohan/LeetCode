f1bf254ef048734b0d790a1ef9a0de84d3d838c3
andy_ruohan <237191099@qq.com>
2022-02-13 21:50:38 +0800

    optimize problem572 Subtree of Another Tree

---
+++ src/Algorithm_II/Day7/problem572/Solution.java
@@ -0,0 +1,39 @@
+package Algorithm_II.Day7.problem572;
+
+public class Solution {
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (dfs(root, subRoot)) {
+            return true;
+        } else {
+            if (root.left != null && root.right != null) {
+                return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+            } else if (root.left != null) {
+                return isSubtree(root.left, subRoot);
+            } else if (root.right != null) {
+                return isSubtree(root.right, subRoot);
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean dfs(TreeNode root, TreeNode subRoot) {
+        if (root != null && subRoot != null) {
+            if (root.val == subRoot.val) {
+                return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
+            } else return false;
+        } else return root == null && subRoot == null;
+    }
+
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2)), new TreeNode(5));
+//        TreeNode subRoot = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+
+        TreeNode root = new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2, new TreeNode(0), null)), new TreeNode(5));
+        TreeNode subRoot = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+
+//        TreeNode root = new TreeNode(1, new TreeNode(1), null);
+//        TreeNode subRoot = new TreeNode(1);
+        System.out.println(isSubtree(root, subRoot));
+    }
+}
