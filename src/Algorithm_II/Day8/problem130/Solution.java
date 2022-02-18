a2e50bced62ed5e466810f438f570d153c186df4
andy_ruohan <237191099@qq.com>
2022-02-18 23:47:27 +0800

    answer to the problem130 Surrounded Regions

---
+++ src/Algorithm_II/Day8/problem130/Solution.java
@@ -0,0 +1,40 @@
+package Algorithm_II.Day8.problem130;
+
+public class Solution {
+    int m, n;
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int x, int y) {
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') {
+            return;
+        }
+
+        board[x][y] = 'A';
+        dfs(board, x - 1, y);
+        dfs(board, x + 1, y);
+        dfs(board, x, y - 1);
+        dfs(board, x, y + 1);
+    }
+}
