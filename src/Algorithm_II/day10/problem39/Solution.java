4334e3641501e21a03daf8dbc9792cd81dcf5e91
andyruohan <237191099@qq.com>
2022-02-22 23:11:49 +0800

    optimize problem39 Combination Sum

---
+++ src/Algorithm_II/day10/problem39/Solution.java
@@ -0,0 +1,46 @@
+package Algorithm_II.day10.problem39;
+
+import java.util.*;
+
+public class Solution {
+    private int length;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        length = candidates.length;
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>(set);
+        if (length == 0) {
+            return result;
+        }
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, 0, path, set, target);
+        return new ArrayList<>(set);
+    }
+
+    public void dfs(int[] candidates, int sum, Deque<Integer> path, Set<List<Integer>> result, int target) {
+        if (sum == target) {
+            List<Integer> pathList = new ArrayList<>(path);
+            Collections.sort(pathList);
+            result.add(pathList);
+            return;
+        } else if (sum > target) {
+            return;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (sum + candidates[i] > target) {
+                break;
+            }
+            path.addLast(candidates[i]);
+            sum += candidates[i];
+            dfs(candidates, sum, path, result, target);
+            sum -= candidates[i];
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
+    }
+}
