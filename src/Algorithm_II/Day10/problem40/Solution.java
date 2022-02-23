a70ec956f2f5f64a1143a6d486424b70c3b8f070
andyruohan <237191099@qq.com>
2022-02-23 23:02:08 +0800

    problem40：结合problem39搭建主体框架，以及problem47对相邻元素修小枝

--- src/Algorithm_II/Day10/problem40/Solution.java
+++ src/Algorithm_II/Day10/problem40/Solution.java
@@ -4,46 +4,32 @@ import java.util.*;
 
 public class Solution {
     private int length;
-    private int sum;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         length = candidates.length;
-        Set<List<Integer>> set = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>(set);
-        if (length == 0) {
-            return result;
-        }
+        List<List<Integer>> result = new ArrayList<>();
+        if (length == 0) return result;
         Deque<Integer> path = new ArrayDeque<>();
-        boolean[] used = new boolean[length];
-        dfs(candidates, path, set, target, used);
-        return new ArrayList<>(set);
+        dfs(candidates, path, result, target, 0);
+        return result;
     }
 
-    public void dfs(int[] candidates, Deque<Integer> path, Set<List<Integer>> result, int target, boolean[] used) {
-        if (sum == target) {
-            List<Integer> pathList = new ArrayList<>(path);
-            Collections.sort(pathList);
-            result.add(pathList);
-            return;
-        } else if (sum > target) {
+    public void dfs(int[] candidates, Deque<Integer> path, List<List<Integer>> result, int target, int idx) {
+        if (0 == target) {
+            result.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = 0; i < length; i++) {
-            if (sum + candidates[i] > target) {
+        for (int i = idx; i < length; i++) {
+            if (candidates[i] > target) {
                 break;
             }
-
-            if (used[i] || i > 0 && candidates[i] == candidates[i - 1] && !used[i - 1]) {
+            if (i > idx && candidates[i] == candidates[i - 1]) {
                 continue;
             }
 
             path.addLast(candidates[i]);
-            sum += candidates[i];
-            used[i] = true;
-            dfs(candidates, path, result, target, used);
-            used[i] = false;
-            sum -= candidates[i];
+            dfs(candidates, path, result, target - candidates[i], i + 1);
             path.removeLast();
         }
     }
