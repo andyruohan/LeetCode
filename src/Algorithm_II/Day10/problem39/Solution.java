879df2ede2b97c9402b4d92a0ddc0c10c0f64e88
andyruohan <237191099@qq.com>
2022-02-23 22:41:28 +0800

    problem40：for循环的起始点、target传参、结果集数据结构优化

--- src/Algorithm_II/Day10/problem39/Solution.java
+++ src/Algorithm_II/Day10/problem39/Solution.java
@@ -7,32 +7,26 @@ public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         length = candidates.length;
-        Set<List<Integer>> set = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>(set);
-        if (length == 0) {
-            return result;
-        }
+        List<List<Integer>> result = new ArrayList<>();
+        if (length == 0) return result;
         Deque<Integer> path = new ArrayDeque<>();
-        dfs(candidates, 0, path, set, target);
-        return new ArrayList<>(set);
+        dfs(candidates, path, result, target, 0);
+        return result;
     }
 
-    public void dfs(int[] candidates, int sum, Deque<Integer> path, Set<List<Integer>> result, int target) {
-        if (sum == target) {
-            List<Integer> pathList = new ArrayList<>(path);
-            Collections.sort(pathList);
-            result.add(pathList);
+    public void dfs(int[] candidates, Deque<Integer> path, List<List<Integer>> result, int target, int idx) {
+        if (0 == target) {
+            result.add(new ArrayList<>(path));
             return;
         }
 
-        for (int i = 0; i < length; i++) {
-            if (sum + candidates[i] > target) {
+        for (int i = idx; i < length; i++) {
+            if (candidates[i] > target) {
                 break;
             }
+
             path.addLast(candidates[i]);
-            sum += candidates[i];
-            dfs(candidates, sum, path, result, target);
-            sum -= candidates[i];
+            dfs(candidates, path, result, target - candidates[i], i);
             path.removeLast();
         }
     }
