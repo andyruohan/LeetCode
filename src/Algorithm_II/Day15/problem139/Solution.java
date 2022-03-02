e8a08dc970fe3ae3c5f474335a426340f982d4ce
andyruohan <237191099@qq.com>
2022-03-02 23:22:56 +0800

    answer to the problem139 Word Break

--- src/Algorithm_II/Day15/problem139/Solution.java
+++ src/Algorithm_II/Day15/problem139/Solution.java
@@ -1,29 +1,39 @@
 package Algorithm_II.Day15.problem139;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int count = 0;
-        Set<String> words = new HashSet<>();
-        for (String str : wordDict) {
-            if (s.contains(str)) {
-                ++count;
-                words.add(str);
+        int length = s.length();
+        boolean[] dp = new boolean[length + 1];
+        dp[0] = true;
+        for (int i = 1; i <= length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
 
-        return count == words.size();
+        return dp[length];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         List<String> wordDict = new ArrayList<>();
-        wordDict.add("leet");
-        wordDict.add("code");
-        System.out.println(solution.wordBreak("leetcode", wordDict));
+//        wordDict.add("cats");
+//        wordDict.add("dog");
+//        wordDict.add("sand");
+//        wordDict.add("and");
+//        wordDict.add("cat");
+//        System.out.println(solution.wordBreak("catsanddog", wordDict));
+//        wordDict.add("aaaa");
+//        wordDict.add("aaa");
+//        System.out.println(solution.wordBreak("aaaaaaa", wordDict));
+        wordDict.add("apple");
+        wordDict.add("pen");
+        System.out.println(solution.wordBreak("applepenapple", wordDict));
     }
 }
