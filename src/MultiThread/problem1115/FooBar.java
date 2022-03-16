a2e092a1bc0504260223e613a5c11c34a9de03a5
andyruohan <237191099@qq.com>
2022-03-16 21:57:48 +0800

    answer to the problem1115 Print FooBar Alternately with reference

---
+++ src/MultiThread/problem1115/FooBar.java
@@ -0,0 +1,35 @@
+package MultiThread.problem1115;
+
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private volatile int n;
+    Semaphore foo = new Semaphore(1);
+    Semaphore bar = new Semaphore(0);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            foo.acquire();
+            printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            bar.acquire();
+            printBar.run();
+            foo.release();
+        }
+    }
+}
