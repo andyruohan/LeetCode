321339ac6945027d5e95fa2630b626a134deac44
andyruohan <237191099@qq.com>
2022-05-08 22:29:07 +0800

    optimize problem1469 with dfs and reference

--- src/Algorithm_III/Day8/problem1469/Solution.java
+++ src/Algorithm_III/Day8/problem1469/Solution.java
@@ -1,34 +1,29 @@
 package Algorithm_III.Day8.problem1469;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 public class Solution {
     List<Integer> resList = new ArrayList<>();
     public List<Integer> getLonelyNodes(TreeNode root) {
-        if (root == null || root.left == null && root.right == null) {
-            return resList;
-        }
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            TreeNode treeNode = queue.poll();
-            if (treeNode.left != null && treeNode.right == null) {
-                resList.add(treeNode.left.val);
-                queue.add(treeNode.left);
-            } else if (treeNode.left == null && treeNode.right != null) {
-                resList.add(treeNode.right.val);
-                queue.add(treeNode.right);
-            } else if (treeNode.left != null && treeNode.right != null) {
-                queue.add(treeNode.left);
-                queue.add(treeNode.right);
-            }
-        }
-
+        dfs(root);
         return resList;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root.left == null && root.right != null) {
+            resList.add(root.right.val);
+        }
+        if (root.left != null && root.right == null) {
+            resList.add(root.left.val);
+        }
+
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            dfs(root.right);
+        }
     }
 
     public static void main(String[] args) {
