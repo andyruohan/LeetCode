12415d5e54521446a7355db5dd0f7ed185241547
andyruohan <237191099@qq.com>
2022-05-10 22:49:57 +0800

    optimize problem582

--- src/Algorithm_III/Day8/problem582/Solution.java
+++ src/Algorithm_III/Day8/problem582/Solution.java
@@ -6,23 +6,32 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
-    HashMap<Integer, Integer> parentMap = new HashMap<>();
+    HashMap<Integer, List<Integer>> parentMap = new HashMap<>();
     List<Integer> resList = new ArrayList<>();
 
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        for (int i = 0; i < pid.size(); i++) {
-            parentMap.put(pid.get(i), ppid.get(i));
+        for (int i = 0; i < ppid.size(); i++) {
+            List<Integer> currentValues = parentMap.get(ppid.get(i));
+            if (currentValues == null) {
+                List<Integer> tempValue = new ArrayList<>();
+                tempValue.add(pid.get(i));
+                parentMap.put(ppid.get(i), tempValue);
+            } else {
+                currentValues.add(pid.get(i));
+            }
         }
 
+        resList.add(kill);
         dfs(kill);
         return resList;
     }
 
     private void dfs(Integer kill) {
-        for (Integer key : parentMap.keySet()) {
-            if (kill.equals(parentMap.get(key))) {
-                resList.add(key);
-                dfs(key);
+        List<Integer> currentValues = parentMap.get(kill);
+        if (currentValues != null) {
+            for (Integer value : currentValues) {
+                resList.add(value);
+                dfs(value);
             }
         }
     }
@@ -30,8 +39,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        System.out.println(solution.killProcess(Arrays.asList(1, 3, 10, 5), Arrays.asList(3, 0, 5, 3), 5));
+        System.out.println(solution.killProcess(Arrays.asList(1, 3, 10, 5), Arrays.asList(3, 0, 5, 3), 5));
 //        System.out.println(solution.killProcess(Arrays.asList(1, 2, 3), Arrays.asList(0, 1, 2), 1));
-        System.out.println(solution.killProcess(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(0, 1, 1, 1, 1), 1));
+//        System.out.println(solution.killProcess(Arrays.asList(1, 2, 3, 4, 5), Arrays.asList(0, 1, 1, 1, 1), 1));
     }
 }
