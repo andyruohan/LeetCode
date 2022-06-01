66bdeb19462d82e5d5fa8894eb49608d02c18e71
andyruohan <237191099@qq.com>
2022-06-01 23:38:22 +0800

    init problem51

---
+++ src/Algorithm_III/Day12/problem51/Solution.java
@@ -0,0 +1,14 @@
+package Algorithm_III.Day12.problem51;
+
+import java.util.List;
+
+public class Solution {
+    private boolean[][] line;
+    private boolean[][] column;
+    private boolean[][] diagonalLine;
+
+    public List<List<String>> solveNQueens(int n) {
+        line = new boolean[n][n]; column = new boolean[n][n]; diagonalLine = new boolean[n][n];
+        
+    }
+}
