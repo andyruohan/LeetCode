f8d286b608f639af7ec3f4341be41ebe169c5ede
23719099@qq.com <Ljx19921101sm>
2022-06-29 23:19:50 +0800

    init problem51 of offer

---
+++ src/Algorithm_III/offerProblem51/Solution.java
@@ -0,0 +1,12 @@
+package Algorithm_III.offerProblem51;
+
+class Solution {
+    public int reversePairs(int[] nums) {
+        int length = nums.length;
+        if (length < 2) {
+            return 0;
+        }
+
+
+    }
+}
