cf0a45ec101754206e11f6c693670b2f5548d04f
andyruohan <237191099@qq.com>
2022-08-06 00:22:36 +0800

    answer to the problem416 with dp

--- src/Algorithm_III/Day20/problem416/Solution.java
+++ src/Algorithm_III/Day20/problem416/Solution.java
@@ -3,38 +3,31 @@ package Algorithm_III.Day20.problem416;
 import java.util.*;
 
 public class Solution {
-    int sum, length;
-    Map<String, Boolean> visitedMap = new HashMap<>();
+    boolean[][] dp;
     public boolean canPartition(int[] nums) {
-        length = nums.length;
-        sum = Arrays.stream(nums).sum();
-        Arrays.sort(nums);
+        int sum = Arrays.stream(nums).sum();
         if (preCheck(nums, sum)) {
-            return dfs(nums, 0, 0);
+            int m = nums.length, n = sum / 2;
+            dp = new boolean[m][n + 1];
+            dp[0][nums[0]] = true;
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dp[i][j] = dp[i - 1][j] || nums[i] == j;
+                    if (j > nums[i]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i]];
+                    }
+                }
+            }
+            return dp[m - 1][n];
         } else {
             return false;
-        }
-    }
-
-    public boolean dfs(int[] nums, int i, int curSum) {
-        String key = curSum + "_" + i;
-        if (visitedMap.containsKey(key)) {
-            return visitedMap.get(key);
-        }
-        if (2 * curSum == sum) {
-            return true;
-        } else if (2 * curSum > sum || i >= length) {
-            return false;
-        } else {
-            visitedMap.put(key, dfs(nums, i + 1, curSum + nums[i]) || dfs(nums, i + 1, curSum));
-            return visitedMap.get(key);
         }
     }
 
     public boolean preCheck(int[] nums, int sum) {
         if (sum % 2 == 1) {
             return false;
-        } else return nums[nums.length - 1] <= sum / 2;
+        } else return nums[0] <= sum / 2;
     }
 
     public static void main(String[] args) {
@@ -43,7 +36,9 @@ public class Solution {
 //        System.out.println(solution.canPartition(new int[]{9,1,2,4,10}));
 //        System.out.println(solution.canPartition(new int[]{100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,99,97}));
 //        System.out.println(solution.canPartition(new int[]{1, 5, 11, 5}));
-        System.out.println(solution.canPartition(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100}));
-        System.out.println(solution.canPartition(new int[]{18,17,18,11,15,4,13,11,9}));
+//        System.out.println(solution.canPartition(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100}));
+//        System.out.println(solution.canPartition(new int[]{18,17,18,11,15,4,13,11,9}));
+        System.out.println(solution.canPartition(new int[]{9, 5}));
+//        System.out.println(solution.canPartition(new int[]{14,9,8,4,3,2}));
     }
 }
