c12b693a888d31e5aead55f9be0538296d457e52
andyruohan <237191099@qq.com>
2025-01-08 22:42:36 +0800

    solution to the problem167 (two-pointer method)

--- src/Algorithm_I/Day3/problem167/Solution.java
+++ src/Algorithm_I/Day3/problem167/Solution.java
@@ -14,6 +14,23 @@ public class Solution {
         return new int[] {-1, -1};
     }
 
+    public static int[] twoSum2(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int sum;
+        while(left < right) {
+            sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+
     public static void main(String[] args) {
         System.out.println(twoSum(new int[] {2,7,11,15}, 9));
     }
