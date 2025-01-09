f095b65fc1ceb75dbfe82dae1ba04b71c747c8f4
andyruohan <237191099@qq.com>
2025-01-09 22:32:31 +0800

    solution to the problem26 (two-pointer method)

--- src/problem26/Solution.java
+++ src/problem26/Solution.java
@@ -20,6 +20,24 @@ public class Solution {
         return slowI + 1;
     }
 
+    public static int removeDuplicates2(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int slow = 0, fast = 1;
+        while (fast < nums.length) {
+            if (nums[slow] == nums[fast]) {
+                fast++;
+            } else if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+                fast++;
+            }
+        }
+        return slow + 1;
+    }
+
     public static void main(String[] args) {
         System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
     }
